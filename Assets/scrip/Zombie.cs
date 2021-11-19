c27abda524b72deee4653255ba7d079abb9a3c90
FARRAS-DARKUNO <83520627+FARRAS-DARKUNO@users.noreply.github.com>
2021-11-19 11:05:53 +0700

    tambah zombies

--- Assets/scrip/Zombie.cs
+++ Assets/scrip/Zombie.cs
@@ -9,10 +9,12 @@ public class Zombie : MonoBehaviour
      private float speed ;
      public float movei ;
      public GameObject player_take;
+     private bool isPlayer ;
     void Start()
     {
         speed = 2;
         movei = 1;
+        isPlayer = false ;
     }
 
     // Update is called once per frame
@@ -20,9 +22,18 @@ public class Zombie : MonoBehaviour
     {
         transform.position += new Vector3(movei,0,0) * Time.deltaTime * -speed ;
 
-        if (this.gameObject.tag == player_take.gameObject.tag){
-            speed = 0;
+        if (isPlayer) speed = 0;
+        else speed = 2 ;
+
+        
+    }
+    private void OnTriggerEnter2D(Collider2D zombies) {
+        if (zombies.gameObject.tag == "Player"){
+            isPlayer = true;
         }
+    }
+    private void OnTriggerExit2D(Collider2D zombies) {
+        isPlayer = false ;
     }
     
 }
