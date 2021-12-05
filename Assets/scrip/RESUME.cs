e5a3ff13b0a68553da906855f5c79f14cf99f3ff
FARRAS-DARKUNO <83520627+FARRAS-DARKUNO@users.noreply.github.com>
2021-12-05 15:45:13 +0700

    biodo amat

---
+++ Assets/scrip/RESUME.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RESUME : MonoBehaviour
+{
+    public GameObject menu ;
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown() {
+        menu.SetActive(false);
+        Time.timeScale = 1 ;
+    }
+}
